fad7c73fb9af4335b9c3a55c5e61491312fc1ff6
Lv Shun <lvshun@tsintergy.com>
2023-10-20 15:02:16 +0800

    feat: 背景图片预加载

--- src/runtime/getInitialState.ts
+++ src/runtime/getInitialState.ts
@@ -3,11 +3,20 @@ import { history } from '@umijs/max';
 
 import localRoutes from '../../config/routes';
 import { getLocalRoutes } from './helper';
+import { theme } from '@/layouts/helper';
 
 
 async function getInitialState(): Promise<InitialState | null> {
   const routes = getLocalRoutes(localRoutes);
-
+    Object.keys(theme).forEach(key => {
+      const url = theme[key]['--background-img'].split('(')[1].split(')')[0]
+      console.log(url);
+      const img = document.createElement('img')
+      img.setAttribute('key', url)
+      img.setAttribute('src', url)
+      img.setAttribute('style', 'display: none')
+      document.documentElement.appendChild(img)
+    })
     return {
       name: 'Gutter',
       routes,
